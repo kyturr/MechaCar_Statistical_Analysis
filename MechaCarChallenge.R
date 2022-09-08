2f840f601a6053bfab5c0a6f1813d16d0ba0a0fb
Kevin <kyturr@gmail.com>
2022-09-07 19:48:08 -0700

    Reuploaded with saved version

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,8 +1,20 @@
+#Deliverable 1
 library(dplyr)
 mcar_df<- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 lm(mpg~ AWD + ground_clearance + spoiler_angle + vehicle_weight + vehicle_length,mcar_df)
 summary(lm(mpg~ AWD + ground_clearance + spoiler_angle + vehicle_weight + vehicle_length,mcar_df))
+
+
+#Deliverable 2
 susp_df<- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
 total_summary<- susp_df %>% summarize(Mean=mean(PSI), Median=median(PSI),Variance=var(PSI),SD=sd(PSI),groups = 'keep')
 lot_summary<-  susp_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI),Variance=var(PSI),SD=sd(PSI),groups = 'keep')
                                       
+#Deliverable 3
+t.test(x=susp_df$PSI,mu=1500) 
+t.test(subset(susp_df,Manufacturing_Lot=="Lot1",select = PSI),
+       mu=1500)
+t.test(subset(susp_df,Manufacturing_Lot=="Lot2",select = PSI),
+       mu=1500)
+t.test(subset(susp_df,Manufacturing_Lot=="Lot3",select = PSI),
+       mu=1500)
